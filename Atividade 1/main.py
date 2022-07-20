50a880fd5b9270cc1ad46701f7141c91b4599cc9
laisy <laisyferreira2011@gmail.com>
2022-07-19 21:36:19 -0300

    Add write file

--- Atividade 1/main.py
+++ Atividade 1/main.py
@@ -1,17 +1,29 @@
 from syntax_analisys import *
 
 def main():
-  a = SyntaxAnalisys(open_arq())
-  a.analyze(open_arq())
-  show_text(a)
+  arq = open('text.txt', 'r')
 
-  print('Done')
+  a = SyntaxAnalisys(arq)
+  a.analyze(arq)
+
+  arq.close()
+  show_text(a)
+  close_arq(a)
+
   return
 
-def open_arq():
-  arq = open('text.txt', 'r')
-  return arq
+def close_arq(a):
+  arq = open('result.txt', 'w')
 
+  arq.write("Verbos: " + str(a.verbos) + "\n")
+  arq.write("Verbos Auxiliares: " + str(a.verbos_aux) + "\n")
+  arq.write("Pronomes: " + str(a.pronomes) + "\n")
+  arq.write("Substantivos Comuns: " + str(a.subst_comuns) + "\n")
+  arq.write("Substantivos Próprios: " + str(a.subst_proprios) + "\n")
+  arq.write("Conjunções: " + str(a.conjuncoes) + "\n")
+
+  arq.close()
+  
 def show_text(a):
   print("Verbos:", a.verbos)
   print("Verbos Auxiliares:", a.verbos_aux)
